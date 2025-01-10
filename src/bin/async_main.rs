f911b2e849a90f250504de83a8de136f38c8ecb2
inadati <dr.inadati@gmail.com>
2025-01-10 22:01:47 +0900

    1st commit

---
+++ src/bin/async_main.rs
@@ -0,0 +1,40 @@
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::prelude::*;
+use log::info;
+
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+
+
+#[main]
+async fn main(spawner: Spawner) {
+    let peripherals = esp_hal::init({
+        let mut config = esp_hal::Config::default();
+        config.cpu_clock = CpuClock::max();
+        config
+    });
+
+
+    esp_println::logger::init_logger_from_env();
+
+    let timer0 = esp_hal::timer::systimer::SystemTimer::new(peripherals.SYSTIMER)
+        .split::<esp_hal::timer::systimer::Target>();
+    esp_hal_embassy::init(timer0.alarm0);
+
+    info!("Embassy initialized!");
+
+
+    // TODO: Spawn some tasks
+    let _ = spawner;
+
+    loop {
+        info!("Hello world!");
+        Timer::after(Duration::from_secs(1)).await;
+    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/v0.22.0/examples/src/bin
+
+}
